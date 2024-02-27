4264830ed921b1ae473bd5bcf4425f44bb454dd8
Adel Ansari <adelansari.a@gmailcom>
2024-02-27 22:48:57 +0200

    some additions

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
+/*
+** Interaction 1 **
+When the user clicks the start button:
+- change the icon to a pause button
+- decrease counter
+- animations start
+
+** Interaction 2 **
+when first timer ends, start break time
+- change the background color to green
+- decrease counter
+
+** Interaction 3 **
+when break time ends
+- stop everything
+
+** Interaction 4 **
+when the user clicks the add 1 minute button
+- add time to the currrent timer
+
+** Interaction 5 **
+As timer decrements
+- rotate the arm
+0 change the lenrgth of the svg circle
+
+** Interaction 6 **
+when the user clicks the pause button
+- animations stop
+- timers stop
+- button changes from pause o play
+
+
+*/
+
+
 const controlBtn = document.querySelector('.control-btn');
 const workTimer = document.querySelector('#workTimer');
 const breakTimer = document.querySelector('#breakTimer');
@@ -20,15 +55,16 @@ currentTimer = workTimer;
 circle.style.strokeDasharray = circumference;
 circle.style.strokeDashoffset = circumference;
 
-updateTimer(workTimer, workSeconds)
-updateTimer(breakTimer, breakSeconds)
-
 const updateTimer = (timer, numSeconds) => {
-    const minutes = Math.floor(numSeconds / 60);
+    const minutes = Math.floor(numSeconds / 60); // integer divisions
     const seconds = numSeconds % 60;
     const formattedSeconds = seconds.toString().padStart(2, '0');
-    timer.textContent = `${minutes}:${formattedSeconds}`;
+    // calculate the time before updating
+    timer.innerText = `${minutes}:${formattedSeconds}`;
 }
+
+updateTimer(workTimer, workSeconds)
+updateTimer(breakTimer, breakSeconds)
 
 controlBtn.addEventListener('click', () => {
     isPaused = !isPaused;
@@ -71,3 +107,12 @@ const setProgress = (newPercentageValue) => {
     const rotation = 360 * newPercentageValue;
     arm.style.transform = `rotate(${rotation}deg)`;
 }
+
+
+addTimeBtn.addEventListener('click', () => {
+    if (!isPaused) {
+        currentSeconds += increment;
+        currentTotal += increment;
+        updateTimer(currentTimer, currentSeconds);
+    }
+});
